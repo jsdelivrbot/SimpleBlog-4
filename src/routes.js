4550a774fc1cff7d88c4020f7481eac89c8ecaab
otsybulsky <helg37@gmail.com>
2018-03-28 19:39:17 +0300

    nesting of routes, render this.props.children

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,20 @@ import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 import App from './components/app'
 
+const Greeting = () => {
+    return <div>Hello there!</div>
+}
+
 export default (
-    <Route path="/" component={App} />
+    <Route path="/" component={App}>
+        <Route path="greet" component={Greeting} /> 
+        <Route path="greet2" component={Greeting} /> 
+        <Route path="greet3" component={Greeting} />    
+    </Route>
 )
+
+// Nested routes
+// /       App
+// /greet  App,Greeting
+// /greet2 App,Greeting
+// /greet3 App,Greeting
