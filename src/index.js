360d9e23be60bee2ecbdf24fb02dc8b32b800d02
otsybulsky <helg37@gmail.com>
2018-03-29 20:56:05 +0300

    action FETCH_POSTS

--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ import { createStore, applyMiddleware } from 'redux';
 import { Router, browserHistory } from 'react-router'
 import reducers from './reducers'
 import routes from './routes'
+import promise from 'redux-promise'
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
