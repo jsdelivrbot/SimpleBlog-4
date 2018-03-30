75139d8b427c801c2e38b533942fe3aee54ec079
otsybulsky <helg37@gmail.com>
2018-03-30 14:52:06 +0300

    create post creator

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { reduxForm } from 'redux-form'
+import { createPost } from '../actions'
 
 class PostsNew extends Component {
   render() {
@@ -8,7 +9,7 @@ class PostsNew extends Component {
     // FormProps={title} === {...title}
 
     return (
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(this.props.createPost)}>
         <h3>Create a new post</h3>
         <div className="form-group">
           <label>Title</label>
@@ -31,10 +32,17 @@ class PostsNew extends Component {
   }
 }
 
-export default reduxForm({
-  form: 'PostsNewForm',
-  fields: ['title', 'categories', 'content']
-})(PostsNew)
+//connect: first argument is mapSrareToProps, 2nd is mapDispatchToProps
+//reduxForm: 1st is form config, 2nd is mapStateToProps, 3rd is mapDispatchToProps
+
+export default reduxForm(
+  {
+    form: 'PostsNewForm',
+    fields: ['title', 'categories', 'content']
+  },
+  null,
+  { createPost }
+)(PostsNew)
 
 // //global application state
 // state ===
