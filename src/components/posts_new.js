b16dd9ea3732aac04cd94e69581ea5b7fa42effc
otsybulsky <helg37@gmail.com>
2018-03-30 13:56:43 +0300

    add redux form to PostsNew component

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,9 +1,45 @@
 import React, { Component } from 'react'
+import { reduxForm } from 'redux-form'
 
 class PostsNew extends Component {
   render() {
-    return <div>Create form</div>
+    return (
+      <form>
+        <h3 Create a new post />
+        <div className="form-group">
+          <label>Title</label>
+          <input type="text" className="form-control" />
+        </div>
+        <div className="form-group">
+          <label>Categories</label>
+          <input type="text" className="form-control" />
+        </div>
+        <div className="form-group">
+          <label>Content</label>
+          <textarea className="form-control" />
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    )
   }
 }
 
-export default PostsNew
+export default reduxForm({
+  form: 'PostsNewForm',
+  fields: ['title', 'categories', 'content']
+})(PostsNew)
+
+// //global application state
+// state ===
+//   {
+//     form: {
+//       PostsNewForm: {
+//         title: '....',
+//         categories: '....',
+//         content: '....'
+//       }
+//     }
+//   }
